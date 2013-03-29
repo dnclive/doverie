2a5bbf6edc8b2122fe5624a82509ab1815d0aceb
dnclive <dnclive@gmail.com>
2013-03-30 02:29:09 +0400

    t_doverie f_action dev

--- res/php/t_doverie.php
+++ res/php/t_doverie.php
@@ -34,22 +34,36 @@
 		t_deb_flog(__LINE__, __FILE__, $param_arr, "t_doverie");
 		t_deb_flog(__LINE__, __FILE__, $url, "t_doverie");
 		
-		if ($cses = curl_init()) 
+		if ($cses = curl_init($url)) 
 		{
+			$headers = array
+			(
+				'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*;q=0.8',
+				'Accept-Language: ru,en-us;q=0.7,en;q=0.3',
+				'Accept-Encoding: deflate',
+				'Accept-Charset: windows-1251,utf-8;q=0.7,*;q=0.7'
+			); 
 			curl_setopt($cses, CURLOPT_SSL_VERIFYPEER, 0);
 			curl_setopt($cses, CURLOPT_SSL_VERIFYHOST, 0);
 			curl_setopt($cses, CURLOPT_SSLCERT, $sslcert);
 			curl_setopt($cses, CURLOPT_SSLCERTPASSWD, $sslcertpasswd);
 			curl_setopt($cses, CURLOPT_CAINFO, $cainfo);
 			curl_setopt($cses, CURLOPT_URL, iconv('utf-8', 'windows-1251', $url));
-			curl_setopt($cses, CURLOPT_HEADER, false);
+			curl_setopt($cses, CURLOPT_HEADER, $headers);
 			curl_setopt($cses, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($cses, CURLOPT_TIMEOUT, 20);
 			curl_setopt($cses, CURLOPT_USERAGENT, 'Ellis GKH web frontend');
+			curl_setopt($cses, CURLOPT_VERBOSE, true);
+			
+			print_r(curl_getinfo($cses));
+		
+			$resp = curl_exec($cses); // выполняем запрос curl
+			curl_close($cses);
 		}
 		
-		$resp = curl_exec($cses); // выполняем запрос curl
-		curl_close($cses);
+		
+		
+		echo($resp);
 		
 		t_deb_flog(__LINE__, __FILE__, $resp, "t_doverie");
 		
